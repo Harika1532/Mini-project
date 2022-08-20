7112eeef36398148faf938fbbdfb8b1e02a551d9
Harika1532 <venkataharika.idamkanti@freshworks.com>
2022-08-21 03:06:54 +0530

    showing and updating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,82 +1,45 @@
 class UsersController < ApplicationController
-    # debugger
-    # user = User.find(user_params)
-    # http_basic_authenticate_with name:"a@a", password:"aaa"
-    # before_action :authenticate, :only => [:show] 
-    # before_action :current_user
-
-    # user = User.find(email: user_params[:email])
-    # user.authenticate(user_params)
+    before_action :authenticate_request, except: [:create]
 
 
-
-    # def user_name_and_password(request)
-    #     debugger
-    #     decode_credentials(request).split(":", 2)
-    # end
     def create
          # debugger
-        user = User.create(user_params)
-        render json: {"user": user_params}, status: 201
-
-        # if user.save
-        #     #debugger
-        #     render json: user, status: 201
-        # else
-        #     render json: { errors: user.errors }, status: 422
-        # end
-        # if user.present? && user.authenticate(params[:password_digest])
-        #     current_user = user.id
-        # end
-
-
-        
-    end
-
-
-
-    # User.find(params[:id])["email"]
-    
-
-
-    
-    def show
-
-        user=User.find(params[:id])
-        
-
-    #      if http_basic_authenticate_or_request_with name:user.email, password:user.password_digest
-            # current_user = user
-    #       # render json: user, status:201
-            render json: {"user"=>user.slice(:id,:email,:password,:role)} 
-        # else
-        #     render json: { errors: user.errors }, status: 422
-        # end
-
-    
-    end
-
-    def update
-        user=User.find(params[:id])
-        if user.update(user_params)
-            render json: user, status:200
+        user = User.new(user_params)
+        if user.save
+            #debugger
+            render json:{"user"=>user.slice(:id,:email,:password_digest, :role)} , status: 201
         else
             render json: { errors: user.errors }, status: 422
         end
     end
+    
+    def show
+        if @current_user.id == params[:id].to_i
+            user=User.find(params[:id])
+            render json: user 
+        else
+            render json: { errors: "denied" }, status: 422
+        end
+    end
 
+    def update
+        if @current_user.id == params[:id].to_i
 
-  
-  def index
-    user=User.all
-    render json: user, status: 200
-  end
-
+            user=User.find(params[:id])
+            if user.update(user_params)
+                render json: user, status:200
+            else
+                render json: { errors: user.errors }, status: 422
+            end
+        else
+            render json: {error: "denied"}, status: 422
+        end
+    end
 
 private
     # debugger
     def user_params
-        params.require(:user).permit(:email,  :password, :password_confirmation, :role)
+        params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
 
